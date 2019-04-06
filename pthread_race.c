8970c7642b616bb8cd74c93ad2ac6ada2bc921f0
Brian Lai <brianlai30@outlook.com>
2019-04-05 23:45:56 -0700

    Whitespace

--- pthread_race.c
+++ pthread_race.c
@@ -29,14 +29,14 @@ void* thread_func(void* arg) {
 
 int main(int argc, char** argv) {
     pthread_t ids[10];
-    int i;
 
-    for(i = 0; i < 10; i++) {
+    int i;
+    for (i = 0; i < 10; i++) {
         int idx = i;
         pthread_create(&ids[i], NULL, thread_func, (void*) &idx);
     }
 
-    for(i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         pthread_join(ids[i], NULL);
     }
 
