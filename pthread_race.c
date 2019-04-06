f44b67e4b39e1f1a361a50ee14b7baa2be99f47f
Brian Lai <brianlai30@outlook.com>
2019-04-06 14:15:26 -0700

    Added add and sub thread funcs. Added final print statement. Filled out funcs, i think im done

--- pthread_race.c
+++ pthread_race.c
@@ -22,23 +22,45 @@
  */
 struct timespec ts = {0, 150000};
 
-void* thread_func(void* arg) {
-    int idx = *((int*) arg);
-    printf("Thread Id : %d\n",idx);
+int global_value = 0;
+
+void* add_thread_func(void* arg) {
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+        int tid = *((int*) arg);
+        int temp = global_value;
+        temp += 10;
+        global_value = temp;
+        printf("Current Value written to Global Variables by ADDER thread id: %d is %d\n", tid, temp);
+        nanosleep(&ts, NULL);
+    }
+}
+
+void* sub_thread_func(void* arg) {
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+        int tid = *((int *) arg);
+        int temp = global_value;
+        temp -= 10;
+        global_value = temp;
+        printf("Current Value written to Global Variables by SUBTRACTOR thread id: %d is %d\n", tid, temp);
+        nanosleep(&ts, NULL);
+    }
 }
 
 int main(int argc, char** argv) {
-    pthread_t ids[10];
+    pthread_t ids[MAX_THREADS];
 
     int i;
     for (i = 0; i < MAX_THREADS; i++) {
         int idx = i;
-        pthread_create(&ids[i], NULL, thread_func, (void*) &idx);
+        pthread_create(&ids[i], NULL, add_thread_func, (void*) &idx);
+        pthread_create(&ids[i], NULL, sub_thread_func, (void*) &idx);
     }
 
     for (i = 0; i < MAX_THREADS; i++) {
         pthread_join(ids[i], NULL);
     }
+
+    printf("Final Value of Shared Variable : %d", global_value);
 
     return 0;
 }
